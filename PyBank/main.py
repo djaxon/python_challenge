58ea48e710d9e359c34858095fbf846432c55484
Dan Jackson <djacks11784@gmail.com>
2020-07-06 15:57:31 -0600

    Updated main.py Bank file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,21 +1,43 @@
 import os
 import csv
 
+Total_Months=[]
+Profit_Sum=[]
+Change_Profit=[]
+Earnings_Total=0
+Earnings_Change=0
+
+
 # Path to collect data from the Resources folder
-budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
-with open(budget_csv) as csvfile:
+budget = os.path.join('Resources','budget_data.csv')
+
+with open(budget) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    print(csvreader)
-
-    # Read the header row first (skip this step if there is now header)
+    # print(csvreader)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
+    for row in csvreader:      
+        
+        Date=row[0]
+        Earnings= row[1]
+        Total_Months.append(Date)
+        Profit_Sum.append(Earnings)
+        Change_Profit.append(Earnings)
 
-    # Read each row of data after the header
-    for row in csvreader:
-        print(row)
+print(f'Financial Analysis')
+print(f'------------------------')
+
+Months=len(Total_Months)
+
+print(f"Total Months: " + str(Months))
+for i in range(0,len(Profit_Sum)):
+    Earnings_Total=Earnings_Total+int(Profit_Sum[i])
+print(f'Total: ${Earnings_Total}')    
+
+
+
+
 
 
